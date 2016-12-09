0a11005a10060cadad12eb82d0dcda03a6e13d9a
Matthias Pueski <matthias@pueski.de>
2016-12-09 21:42:43 +0100

    Code cleanup

---
+++ Source/ModTarget.h
@@ -0,0 +1,14 @@
+//
+//  ModTarget.hpp
+//  Trio
+//
+//  Created by Matthias Pueski on 09.12.16.
+//
+//
+
+#ifndef ModTarget_hpp
+#define ModTarget_hpp
+
+#include <stdio.h>
+
+#endif /* ModTarget_hpp */
